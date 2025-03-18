fed8d26bb23417d692e985840fa3a0feb0d9e188
IleaMario <ileamario71@gmail.com>
2025-03-18 21:10:25 +0200

    revert project changes

--- 01_Tasks/src/main/java/tasks/services/IO_Errors.java
+++
@@ -1,8 +0,0 @@
-package tasks.services;
-
-public class IO_Errors {
-    // add errors messages here
-    public static final String ERROR_TASK_NULL = "Task shouldn't be null";
-    public static final String ERROR_IO_EXCEPTION  = "IO exception reading or writing file";
-
-}
